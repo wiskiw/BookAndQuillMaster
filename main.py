7b5e309fdf8316c47b62e0fac3afba5dfb6fe898
Andrei Yablonski <9986008+wiskiw@users.noreply.github.com>
2024-08-12 11:30:05 +0200

    Add comments to main.py

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ from bookmaster.text_unit_reader import TextUnitReader
 char_width_dict_file = 'bookmaster/char_width.txt'
 export_text_unit_file = './debug/text_units.json5'
 input_file = './debug/input.txt'
+output_json_file = './debug/book_json.json'
+output_pretty_file = './debug/book_json.json'
 
 
-def run():
+# This script gives you a basic usage example.
+# It takes input from #input_file
+# And generates a text to fit in a book in two formats:
+# Take a look on output_json_file and output_pretty_file
+#
+# run command: python3 main.py
+
+def __run():
     # reading input
     raw_text = read_file(file_path=input_file)
 
@@ -28,11 +37,11 @@ def run():
 
     # writing output
     book_formatter = McBookFormatter(book)
-    write_json('./debug/book_json.json', book_formatter.to_json())
-    write_file('./debug/book_pretty.txt', book_formatter.to_pretty_text())
+    write_json(output_json_file, book_formatter.to_json())
+    write_file(output_pretty_file, book_formatter.to_pretty_text())
 
     move_to_bookcopy_dir('./debug/book_json.json', 'book_json.json')
 
 
 if __name__ == '__main__':
-    run()
+    __run()
